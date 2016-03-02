c07fa7837143279f6917f74dc45a26f6e6f4e625
Hoid <tlcheek@ncsu.edu>
2016-03-01 20:46:43 -0500

    Add Riot API calls and JSON response info to UI

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import sys
 from PyQt4.QtGui import QApplication, QMainWindow
 from PyQt4 import uic
+import requests,  json
 
 import showMainWindow
+
+summonerName = ""
+summonerId = ""
+apiKey = "9a5609ad-2d90-4aff-a09f-dc86632f3770"
 
 class MainWindow(QMainWindow):
     
     def __init__(self):
         super(QMainWindow,  self).__init__()
         self.ui = uic.loadUi('C:/Users/cheek/Documents/Code/LoL-Performance-Tracker/MainWindow.ui')
+        
+        summonerInfoResponse = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Transatlantacism?api_key=" + apiKey)
+        summonerInfoResponse = json.loads(summonerInfoResponse.text)
+        summonerName = "transatlantacism"
+        summonerId = summonerInfoResponse[summonerName]["id"]
+        
+        self.ui.summonerNameLabel.setText(str(summonerId))
+        
         self.ui.show()
-
-
 
 
 def main():
--- showMainWindow.py
+++ showMainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file './MainWindow.ui'
+# Form implementation generated from reading ui file 'MainWindow.ui'
 #
 # Created by: PyQt4 UI code generator 4.11.4
 #
@@ -25,14 +25,17 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(1776, 1117)
+        MainWindow.resize(1706, 1120)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.mainTabView = QtGui.QTabWidget(self.centralwidget)
-        self.mainTabView.setGeometry(QtCore.QRect(60, 160, 1661, 841))
+        self.mainTabView.setGeometry(QtCore.QRect(50, 160, 1611, 871))
         self.mainTabView.setStatusTip(_fromUtf8(""))
         self.mainTabView.setWhatsThis(_fromUtf8(""))
+        self.mainTabView.setTabPosition(QtGui.QTabWidget.North)
+        self.mainTabView.setTabShape(QtGui.QTabWidget.Rounded)
         self.mainTabView.setIconSize(QtCore.QSize(32, 32))
+        self.mainTabView.setElideMode(QtCore.Qt.ElideNone)
         self.mainTabView.setObjectName(_fromUtf8("mainTabView"))
         self.soloQueueTab = QtGui.QWidget()
         self.soloQueueTab.setObjectName(_fromUtf8("soloQueueTab"))
@@ -40,9 +43,15 @@ class Ui_MainWindow(object):
         self.compareTab = QtGui.QWidget()
         self.compareTab.setObjectName(_fromUtf8("compareTab"))
         self.mainTabView.addTab(self.compareTab, _fromUtf8(""))
+        self.groupBox = QtGui.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(50, 30, 1611, 111))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.summonerNameLabel = QtGui.QLabel(self.groupBox)
+        self.summonerNameLabel.setGeometry(QtCore.QRect(70, 40, 471, 51))
+        self.summonerNameLabel.setObjectName(_fromUtf8("summonerNameLabel"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1776, 38))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1706, 38))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
@@ -58,4 +67,6 @@ class Ui_MainWindow(object):
         self.mainTabView.setToolTip(_translate("MainWindow", "<html><head/><body><p>Solo Queue</p></body></html>", None))
         self.mainTabView.setTabText(self.mainTabView.indexOf(self.soloQueueTab), _translate("MainWindow", "Solo Queue", None))
         self.mainTabView.setTabText(self.mainTabView.indexOf(self.compareTab), _translate("MainWindow", "Summoner Comparison", None))
+        self.groupBox.setTitle(_translate("MainWindow", "Summoner Info", None))
+        self.summonerNameLabel.setText(_translate("MainWindow", "Stuff", None))
 
