453aa7df2cbf5df953d0a69dc56c8686e22b16ef
Hoid <tlcheek@ncsu.edu>
2016-03-05 22:02:30 -0500

    Add checks for response codes and reformat summoner name user input before use

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import requests,  json
 import showMainWindow,  config
 
 summonerName = ""
+summonerNameFull = ""
 summonerRegion = ""
 summonerId = ""
 apiKey = "9a5609ad-2d90-4aff-a09f-dc86632f3770"
@@ -29,37 +30,59 @@ class MainWindow(QMainWindow):
         summonerRank = self.getSummonerRank()
         
         if summonerName:
-            self.ui.summonerNameLabel.setText(summonerName)
+            self.ui.summonerNameLabel.setText(summonerNameFull)
         if summonerRank:
             self.ui.summonerRank.setText(summonerRank)
         
         self.ui.show()
 
     def getSummonerInfo(self):
-        global summonerId,  summonerName
+        global summonerId,  summonerName,  summonerNameFull
         requestURL = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey
         summonerInfoResponse = requests.get(requestURL)
-        summonerInfoResponse = json.loads(summonerInfoResponse.text)
-        summonerId = summonerInfoResponse[summonerName]["id"]
-        summonerName = summonerInfoResponse[summonerName]["name"]
+        responseMessage = self.checkResponseCode(summonerInfoResponse)
+        if responseMessage == "ok":
+            summonerInfoResponse = json.loads(summonerInfoResponse.text)
+            summonerId = summonerInfoResponse[summonerName]["id"]
+            summonerNameFull = summonerInfoResponse[summonerName]["name"]
+        else:
+            print responseMessage
 
     def getSummonerRank(self):
         summonerIdStr = str(summonerId)
         requestURL = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/" + summonerIdStr + "/entry?api_key=" + apiKey
         summonerInfoResponse = requests.get(requestURL)
-        summonerInfoResponse = json.loads(summonerInfoResponse.text)
-        summonerTier = summonerInfoResponse[summonerIdStr][0]["tier"].lower().capitalize()
-        summonerDivision = summonerInfoResponse[summonerIdStr][0]["entries"][0]["division"]
-        summonerLp = summonerInfoResponse[summonerIdStr][0]["entries"][0]["leaguePoints"]
-        summonerRank = summonerTier + " " + summonerDivision + ", " + str(summonerLp) + "lp"
-        return summonerRank
+        responseMessage = self.checkResponseCode(summonerInfoResponse)
+        if responseMessage == "ok":
+            summonerInfoResponse = json.loads(summonerInfoResponse.text)
+            summonerTier = summonerInfoResponse[summonerIdStr][0]["tier"].lower().capitalize()
+            summonerDivision = summonerInfoResponse[summonerIdStr][0]["entries"][0]["division"]
+            summonerLp = summonerInfoResponse[summonerIdStr][0]["entries"][0]["leaguePoints"]
+            summonerRank = summonerTier + " " + summonerDivision + ", " + str(summonerLp) + "lp"
+            return summonerRank
+        else:
+            print responseMessage
+            return "Could not get summoner rank"
+
+    def checkResponseCode(self,  response):
+        codes = {
+            200: "ok", 
+            400: "bad request", 
+            401: "unauthorized", 
+            404: "entity not found", 
+            429: "rate limit exceeded", 
+            500: "internal server error", 
+            503: "service unavailable"
+        }
+        responseMessage = codes.get(response.status_code,  "code not recognized")
+        return responseMessage
 
     def showSummonerNameInputBox(self):
         text, ok = QInputDialog.getText(self, 'Summoner info', 
             'Enter your summoner name:')
         if ok:
             global summonerName
-            summonerName = str(text)
+            summonerName = str(text).replace(" ", "").lower()
         else:
             self.closeApplication()
 
