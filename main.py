e8e73fa2760456bc8dff02d3321e49b2ee16e50a
Hoid <tlcheek@ncsu.edu>
2016-03-09 15:27:20 -0500

    Pull match history worker thread into new file
    Add a new file called workerThreads.py. Pull the class
    getMatchHistoryWorkerThread into this file and add code in this class that
    pulls information like the apiKey and summonerId from config file, rather
    than from a global.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import requests,  json
 from ConfigParser import SafeConfigParser
 
 import showMainWindow
+from workerThreads import getMatchHistoryWorkerThread
 
 summonerName = ""
 summonerNameFull = ""
@@ -82,6 +83,7 @@ class MainWindow(QMainWindow):
             config.set('main',  'summonerId',  str(summonerId))
             config.set('main',  'summonerRank',  summonerRank)
             config.set('main',  'isFirstTimeOpening',  'False')
+            config.set('main',  'apiKey',  apiKey)
             with open(configFileLocation, 'w') as f:
                 config.write(f)
         else: 
@@ -153,69 +155,7 @@ class MainWindow(QMainWindow):
             event.ignore()
 
 
-class getMatchHistoryWorkerThread(QThread):
-    
-    def __init__(self):
-        super(getMatchHistoryWorkerThread,  self).__init__()
-    
-    def run(self):
-        global summonerName, summonerNameFull, summonerId, summonerRegion,  summonerRank
-        self.getMatchHistoryDetails()
-    
-    def getMatchHistory(self):
-        requestURL = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/" + str(summonerId) + "?seasons=SEASON2016&api_key=" + apiKey
-        matchHistoryResponse = requests.get(requestURL)
-        responseMessage = self.checkResponseCode(matchHistoryResponse)
-        if responseMessage == "ok":
-            
-            # write match history in json form to a file
-            matchHistoryResponse = matchHistoryResponse.text
-            fileLocation = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-            fileLocation = fileLocation + '\match_history.txt'
-            f = open(fileLocation,  'w')
-            json.dump(matchHistoryResponse,  f)
-            
-            #decode match history from json and return it
-            matchHistoryResponse = json.loads(matchHistoryResponse)
-            return matchHistoryResponse
-            
-        else:
-            print responseMessage
-            return "Could not get match history"
 
-    def getMatchHistoryDetails(self):
-        matchHistory = self.getMatchHistory()
-        matchHistory = matchHistory["matches"]
-        matchHistoryDetails = [] # For now, just make a new match history each time we start the program. Afterwards, read match history from file and only load the matches that are new.
-        for match in range(5):
-            matchId = matchHistory[match]["matchId"]
-            requestURL = "https://na.api.pvp.net/api/lol/na/v2.2/match/" + str(matchId) + "?api_key=" + apiKey
-            matchDetailsResponse = requests.get(requestURL)
-            responseMessage = self.checkResponseCode(matchDetailsResponse)
-            if responseMessage == "ok":
-                matchDetailsResponse = json.loads(matchDetailsResponse.text)
-                matchHistoryDetails.append(matchDetailsResponse)
-            else:
-                print "For match " + matchId + ", " + responseMessage
-        matchHistoryDetailsJson = json.dumps(matchHistoryDetails)
-        fileLocation = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        fileLocation = fileLocation + '\match_history_details.txt'
-        f = open(fileLocation,  'w')
-        json.dump(matchHistoryDetailsJson,  f)
-        print "Populated match_history_details"
-    
-    def checkResponseCode(self,  response):
-        codes = {
-            200: "ok", 
-            400: "bad request", 
-            401: "unauthorized", 
-            404: "entity not found", 
-            429: "rate limit exceeded", 
-            500: "internal server error", 
-            503: "service unavailable"
-        }
-        responseMessage = codes.get(response.status_code,  "code not recognized")
-        return responseMessage
 
 def main():
     app = QApplication(sys.argv)
---
+++ workerThreads.py
@@ -0,0 +1,81 @@
+import sys,  os
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import requests,  json
+from ConfigParser import SafeConfigParser
+
+import showMainWindow
+
+
+class getMatchHistoryWorkerThread(QThread):
+    
+    def __init__(self):
+        super(getMatchHistoryWorkerThread,  self).__init__()
+        
+    
+    def run(self):
+        global apiKey
+        configFileLocation = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        configFileLocation = configFileLocation + "\config.ini"
+        config = SafeConfigParser()
+        config.read(configFileLocation)
+        summonerId = config.get('main',  'summonerId')
+        apiKey = config.get('main',  'apiKey')
+        
+        self.getMatchHistoryDetails(summonerId)
+    
+    def getMatchHistory(self,  summonerId):
+        requestURL = "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/" + str(summonerId) + "?seasons=SEASON2016&api_key=" + apiKey
+        matchHistoryResponse = requests.get(requestURL)
+        responseMessage = self.checkResponseCode(matchHistoryResponse)
+        if responseMessage == "ok":
+            
+            # write match history in json form to a file
+            matchHistoryResponse = matchHistoryResponse.text
+            fileLocation = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+            fileLocation = fileLocation + '\match_history.txt'
+            f = open(fileLocation,  'w')
+            json.dump(matchHistoryResponse,  f)
+            
+            #decode match history from json and return it
+            matchHistoryResponse = json.loads(matchHistoryResponse)
+            return matchHistoryResponse
+            
+        else:
+            print responseMessage
+            return "Could not get match history"
+
+    def getMatchHistoryDetails(self,  summonerId):
+        matchHistory = self.getMatchHistory(summonerId)
+        matchHistory = matchHistory["matches"]
+        matchHistoryDetails = [] # For now, just make a new match history each time we start the program. Afterwards, read match history from file and only load the matches that are new.
+        for match in range(5):
+            matchId = matchHistory[match]["matchId"]
+            requestURL = "https://na.api.pvp.net/api/lol/na/v2.2/match/" + str(matchId) + "?api_key=" + apiKey
+            matchDetailsResponse = requests.get(requestURL)
+            responseMessage = self.checkResponseCode(matchDetailsResponse)
+            if responseMessage == "ok":
+                matchDetailsResponse = json.loads(matchDetailsResponse.text)
+                matchHistoryDetails.append(matchDetailsResponse)
+            else:
+                print "For match " + matchId + ", " + responseMessage
+        matchHistoryDetailsJson = json.dumps(matchHistoryDetails)
+        fileLocation = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        fileLocation = fileLocation + '\match_history_details.txt'
+        f = open(fileLocation,  'w')
+        json.dump(matchHistoryDetailsJson,  f)
+        print "Populated match_history_details"
+    
+    def checkResponseCode(self,  response):
+        codes = {
+            200: "ok", 
+            400: "bad request", 
+            401: "unauthorized", 
+            404: "entity not found", 
+            429: "rate limit exceeded", 
+            500: "internal server error", 
+            503: "service unavailable"
+        }
+        responseMessage = codes.get(response.status_code,  "code not recognized")
+        return responseMessage
